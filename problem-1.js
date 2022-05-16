6978593a0c45bc0201dd0de9c610ebee65bc0ba3
mdxshakil <mdxshakil1767@gmail.com>
2022-05-16 15:13:15 +0600

    first commit

---
+++ problem-1.js
@@ -0,0 +1,71 @@
+//Problem 1: Ana to vori conversion
+function anaToVori(ana) {
+    if (typeof ana == 'number' && ana > 0) {
+        const vori = ana / 16;
+        return vori;
+    }
+    else{
+        return 'Please provide a valid input!'
+    }
+}
+var myVori = anaToVori(200);
+console.log(myVori);
+
+//Problem 2: 
+const eachSingaraPrice = 7;
+const eachSomuchaPrice = 10;
+const eachJilapiPrice = 15;
+function pandaCost(singaraQty, somuchaQty, jilapiQty ) {
+    if ((singaraQty>=0 && somuchaQty>=0 && jilapiQty>=0) && typeof singaraQty=='number' && typeof somuchaQty=='number' && typeof jilapiQty=='number') {
+        const totalCost = (singaraQty*eachSingaraPrice) + (somuchaQty*eachSomuchaPrice) + (jilapiQty*eachJilapiPrice);
+        return totalCost;
+    }
+    else{
+        return 'Please provide valid input!'
+    }
+}
+
+//problem 3: picnic Budget Calculation
+const first100Cost = (100*5000);
+const second100Cost = (100*4000);
+function picnicBudget(totalPerson) {
+    if (totalPerson >=0 && totalPerson <= 100 && typeof totalPerson == 'number') {
+       const totalCost = totalPerson * 5000;
+       return totalCost;
+    }
+    else if (totalPerson > 100 && totalPerson <= 200 && typeof totalPerson == 'number' ) {
+        const remainingPeople = totalPerson - 100;
+        const totalCost = (remainingPeople*4000) + first100Cost;
+        return totalCost;
+    }
+    else if(totalPerson > 200 && typeof totalPerson == 'number'){
+        const remainingPeople = totalPerson - 200;
+        const totalCost = (remainingPeople*3000) + first100Cost + second100Cost;
+        return totalCost;
+    }
+    else{
+        return 'Provide a valid number!'
+    }
+}
+var picnicCost = picnicBudget('s');
+console.log(picnicCost);
+
+//Problem 4:
+var friendNames = ['shakib', 'musfiq', 'fizz', 'tamim', 'liton', 'taskins']
+function oddFriend(names) {
+    let oddLengthName = names[0];
+    if (typeof names == 'object') {
+        for (let i = 0; i < names.length; i++) {
+            const eachName = names[i];
+            if (eachName.length%2 != 0) {
+                oddLengthName = (eachName);
+                return oddLengthName;
+            }
+        }
+    }
+    else{
+        return 'Invalid Input!'
+    }
+}
+var names = oddFriend(friendNames);
+console.log(names);
